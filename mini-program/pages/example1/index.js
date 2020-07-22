4a6ef600003250bfca772442f4415d3dd03b5275
jiaowoxiaobala <415313629@qq.com>
2020-07-22 22:15:08 +0800

    20200722

--- mini-program/pages/example1/index.js
+++ mini-program/pages/example1/index.js
@@ -5,61 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isAuthorization: false
+    isAuthorization: true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    // 获取用户当前设置
+  handleSaveImg() {
+    // 保存前先获取用户授权信息
     wx.getSetting({
-      // 解构赋值直接获取authSetting对象
       success: ({ authSetting }) => {
-        if (authSetting && !authSetting['scope.writePhotosAlbum']) {
-          // 调用授权的弹窗
+        // 如果没有授权
+        if (!authSetting['scope.writePhotosAlbum']) {
+          // 弹出授权框
           wx.authorize({
             scope: 'scope.writePhotosAlbum',
-            success: res => {
+            // 授权成功调用保存图片API
+            success: () => {
+              wx.saveImageToPhotosAlbum({
+                filePath: '/images/wechat.jpg',
+              })
+            },
+            // 授权失败弹窗提示
+            fail: () => {
+              wx.showToast({
+                title:'请授权',
+                icon: 'none'
+              })
               this.setData({
                 isAuthorization: false
               })
-            },
-            fail: res => {
-              this.setData({
-                isAuthorization: true
-              })
             }
-          })
-        }
-      }
-    })
-  },
-  // 也可以用wx.openSetting调起授权设置页面
-  // 这个方法会在打开授权设置页后退时调用
-  handleOpensetting({ detail }) {
-    const { authSetting } = detail;
-    if (authSetting && authSetting['scope.writePhotosAlbum']) {
-      this.setData({
-        isAuthorization: false
-      })
-    }
-  },
-  handleSaveImg() {
-    wx.getSetting({
-      success: ({ authSetting }) => {
-        console.log(authSetting)
-        if (!authSetting['scope.writePhotosAlbum']) {
-          wx.showToast({
-            title: '未获取用户授权',
           })
         } else {
           wx.saveImageToPhotosAlbum({
-            filePath: '/images/wechat.jpg'
+            filePath: '/images/wechat.jpg',
           })
         }
       }
     })
+  },
+  // 在打开授权设置页后返回时触发
+  // 对象嵌套属性解构
+  handleOpensetting({detail: { authSetting }}) {
+    if (authSetting && authSetting['scope.writePhotosAlbum']) {
+      this.setData({
+        isAuthorization: true
+      })
+    }
   }
+
 })
 // 注: 设置界面只会出现小程序已经向用户请求过的权限
---
+++ mini-program/pages/example2/index.js
@@ -0,0 +1,35 @@
+// pages/example2/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    obj: {
+      name: 'xiaoba',
+      age: 22
+    }
+  },
+  handleBtnClick() {
+    this.setData({
+      // 对于对象或数组字段,可以直接修改一个其下的子字段,这样做通常比修改整个对象
+      // 或数组更好
+      'obj.name':'xiaobala'
+    }, () => {
+      // setData第二个参数是回调函数,在setData引起界面更新渲染完毕后执行
+      console.log(this.data.obj)
+    })
+  },
+  // 点击设置open-type="share"的按钮时触发
+  onShareAppMessage(res) {
+    console.log(res)
+    // 通过设置返回的对象可自定义转发内容
+    return {
+      title: '这是我的电子名片,请惠存',
+      path: '/pages/example2',
+      // 定义转发的图片
+      imageUrl: '/images/wechat.jpg'
+    }
+  }
+})
+// 注: 只有定义了此事件处理函数,右上角菜单才会显示'转发'按钮
