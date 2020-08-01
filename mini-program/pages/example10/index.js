24fb68300e71dbca80eef519474fb6f34a1a2fd1
jiaowoxiaobala <415313629@qq.com>
2020-08-01 22:22:25 +0800

    20200801

--- mini-program/pages/example10/index.js
+++ mini-program/pages/example10/index.js
@@ -1,10 +1,24 @@
 // pages/example10/index.js
 const behavior = require('../../components/example/Behavior');
 Component({
+  data: {
+    name: 'xiaoba'
+  },
   behaviors: [behavior],
   methods: {
     onLoad() {
       console.log('页面example10展示了~')
+    },
+    handleTrigger(e) {
+      console.log(e);
+    },
+    handleBtnClick() {
+      // selectComponent调用时要传入一个匹配选择器
+      const com = this.selectComponent('.custom-component');
+      console.log(com);
+      this.setData({
+        name: 'xiaobaba'
+      })
     }
   }
 })
